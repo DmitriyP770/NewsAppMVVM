0df1bf5de08c9aefbfe09f5f54b2be88b23770cb
DmitriyP770 <pigasov.dm@gmail.com>
2022-07-30 13:12:33 +0300

    Add Navigation

--- app/src/main/java/com/example/newsappmvvm/MainActivity.kt
+++ app/src/main/java/com/example/newsappmvvm/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.newsappmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsappmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = findViewById<View>(R.id.newsNavHostFragment)
+
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
     }
 }
